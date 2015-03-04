2ef50ed53efe0035a2670dfc9e2faea706ca9590
Santosh <santosh.ksingh03@gmail.com>
2015-03-04 21:36:22 +0530

    Template Pattern another example

---
+++ TemplateII/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateII
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var systemOperator = new SystemOperator
+            {
+                CellPhone = "145616456",
+                Email = "system@operator.com",
+                Name = "Super Operator",
+                Pager = "465565456"
+            };
+            var message = new Message();
+
+            message.Sender = new SmsNotificationSender(systemOperator);
+            message.Send();
+
+            message.Sender = new MailNotificationSender(systemOperator);
+            message.Send();
+        }
+    }
+}
